e351cb436ea4ec70e64a4ec3a65718269f278f9c
naveengaur <naveengaur212@gmail.com>
2018-06-03 06:24:32 +1000

    Changed Company Dashboard.Clean up of code is needed along with one minor bug fix.

--- app/src/main/java/com/hixel/hixel/company/CompanyActivity.java
+++ app/src/main/java/com/hixel/hixel/company/CompanyActivity.java
@@ -3,8 +3,18 @@ package com.hixel.hixel.company;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.hixel.hixel.R;
 import com.hixel.hixel.models.Company;
 
@@ -65,6 +75,46 @@ public class CompanyActivity extends AppCompatActivity implements CompanyContrac
         liquidityScore.setText(getValue(ratios1.get(0), 2017));
         leverageScore.setText(getValue(ratios1.get(1), 2017));
         healthScore.setText(getValue(ratios1.get(2), 2017));
+        // For the line chart
+        BarChart barChart=findViewById(R.id.lineChart);
+        barChart.setDrawBarShadow(false);
+        barChart.setDrawValueAboveBar(true);
+        barChart.setMaxVisibleValueCount(100);
+        barChart.setPinchZoom(true);
+        barChart.setDrawGridBackground(true);
+
+        // values for the Bar chart
+        ArrayList<BarEntry> barEntries=new ArrayList<>();
+        barEntries.add(new BarEntry(1,120000f));
+        barEntries.add(new BarEntry(2,144000f));
+        barEntries.add(new BarEntry(3,130000f));
+        barEntries.add(new BarEntry(4,36000f));
+
+        BarDataSet barDataSet=new BarDataSet(barEntries,"Data set");
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        BarData data=new BarData(barDataSet);
+        data.setBarWidth(0.9f);
+
+        barChart.setData(data);
+
+        // create X axis
+        String[] financial={"","","","",""};
+        XAxis xAxis=barChart.getXAxis();
+        xAxis.setValueFormatter(new MyXaxisValueFormatter(financial));
+
+        // set up the list view
+        ListView listView =findViewById(R.id.listView);
+        // call the method to setup the values
+        ArrayList<String> ratiosList=new ArrayList<>();
+        ratiosList.add(getValue(ratios1.get(0),2017));
+        ratiosList.add(getValue(ratios1.get(1),2017));
+        ratiosList.add(getValue(ratios1.get(2),2017));
+        ratiosList.add(getValue(ratios1.get(3),2017));
+        ratiosList.add(getValue(ratios1.get(4),2017));
+
+
+        CompanyAdapter adapter=new CompanyAdapter(this,R.layout.adpater_view_layout,ratiosList);
+        listView.setAdapter(adapter);
 
 
     }
@@ -85,7 +135,18 @@ public class CompanyActivity extends AppCompatActivity implements CompanyContrac
            return value;
        }
     }
+public class  MyXaxisValueFormatter implements IAxisValueFormatter
+{
+    String[] mValues;
+    @Override
+    public String getFormattedValue(float value, AxisBase axis) {
+        return mValues[(int)value];
+    }
 
+    public MyXaxisValueFormatter(String[] values) {
+        this.mValues=values;
+    }
+}
 
 
 }
